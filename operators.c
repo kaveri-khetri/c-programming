7a8a8c1ea5fd09c6e577982cdd83fc08f5c37293
kaveri-khetri <khetrikaveri1@gmail.com>
2025-07-10 14:10:40 +0000

    10\7

---
+++ operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>       
+int main(){
+    int a,b;
+
+    printf("enter the first number:");
+    scanf("%d",&a);
+    printf("enter the second number:");
+    scanf("%d",&b);
+
+    printf("add=%d\n",a+b);
+    printf("sub=%d\n",a-b);
+    printf("mul=%d\n",a*b);
+    printf("div=%d\n",a/b);
+    printf("mod=%d\n",a%b);
+}
